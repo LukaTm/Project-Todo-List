09e9fe91e55f4162817009c81ad2c0dcdb77bd7d
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-02-15 23:09:53 +0200

    Change priority with colors

---
+++ src/form.js
@@ -0,0 +1,20 @@
+const buttonIds = ['low', 'medium', 'high'];
+const buttons = buttonIds.map((id) => document.querySelector(`#${id}`));
+console.log(buttons)
+
+export function formButtons() {
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', () => {
+      Checker();
+      button.style.backgroundColor = (button.id === 'low') ? 'green' : (button.id === 'medium') ? 'rgb(212, 212, 76)' : 'red';
+    });
+  }
+}
+
+const Checker = () => {
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.style.backgroundColor = 'rgb(154, 194, 209)';
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { mainPage } from './main-page.js';
 import { addButton } from './add-button.js';
 import { closePopUp } from './add-button.js';
+import { formButtons } from './form.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    formButtons();
 	mainPage();
-    addButton()
-    closePopUp()
+    addButton();
+    closePopUp();
 });
 
