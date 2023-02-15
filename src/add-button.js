f5170b03de0f47a04a7241710ec28b1864be58f9
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-02-15 15:49:26 +0200

    Open PopUp screen and Close works

--- src/add-button.js
+++ src/add-button.js
@@ -4,26 +4,19 @@ export function addButton() {
 	const plusBtn = document.querySelector('#addButton')
 	
 	plusBtn.addEventListener('click', () => {
-		popUp.style.opacity = 1;
+		popUp.style.display = 'grid'
+
 	});
 }
 
 export function closePopUp() {
 	const closePopUp = document.querySelector('.close-btn')
 	closePopUp.addEventListener('click', () => {
-		popUp.style.opacity = '0'
+		popUp.style.display = 'none'
 	});
 }
 
-const closePopOutsideBox = document.querySelector('body')
-const popUpOpacity = document.querySelector('.pop-up')
 
-closePopOutsideBox.addEventListener('click', () => {
-	if (popUp.style.opacity === '1'){
-		closePopOutsideBox.style.opacity = '0.4'
-		popUpOpacity.style.opacity = '1'
-	}
-});
 
 
 
